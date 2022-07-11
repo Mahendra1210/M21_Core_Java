5b0b0f540ada9bd7f68112aaffef4b6c7116e5b0
Mahendra patel <Mahendra patel@HP-1210>
2022-07-11 21:29:41 +0530

    3 commit

---
+++ Day1_GitHub/src/com/capegemini/intro/Program2_addition.java
@@ -0,0 +1,15 @@
+package com.capegemini.intro;
+
+public class Program2_addition {
+
+	public static void main(String[] args) {
+	int a=2;
+	int b=3;
+	int c=a+b;
+	
+	System.out.println("The value of c is:" +c);
+		
+
+	}
+
+}

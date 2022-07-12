256c625c08e07832140fba0068ddff55b0ff5862
Mahendra patel <Mahendra patel@HP-1210>
2022-07-12 19:23:00 +0530

    2 commit

---
+++ Day2_Intro_to_java/src/com/cg/java/Program1.java
@@ -0,0 +1,19 @@
+package com.cg.java;
+
+import java.util.Scanner;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in);
+			System.out.print("Enter the value of a: ");
+		
+				int a=sc.nextInt();
+	
+					System.out.println("The value of a is:" +a);
+						sc.close();
+		
+	}
+
+}
---
+++ Day2_Intro_to_java/src/com/cg/java/Program2.java
@@ -0,0 +1,17 @@
+package com.cg.java;
+
+import java.util.Scanner;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+			System.out.print("Enter the value of d: ");
+				double d=sc.nextDouble();
+					System.out.println("The value of d is: " +d);
+						sc.close();
+		
+
+	}
+
+}
